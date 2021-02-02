0f4e0f1651aef2b40a8bfc5b8139bc94d14cbc72
nely de faria <nelly.nf54@gmail.com>
2021-02-02 11:53:49 -0300

    Usando variável argv na posição 1

--- php-doctrine/commands/criar-aluno.php
+++ php-doctrine/commands/criar-aluno.php
@@ -6,12 +6,11 @@ use Alura\Doctrine\Helper\EntityManagerFactory;
 require_once __DIR__ .  '/../vendor/autoload.php';
 
 $aluno = new Aluno();
-$aluno->setNome('Vinícius Dias');
+$aluno->setNome($argv[1]);
 
 $entityManagerFactory = new EntityManagerFactory();
 $entitiManager = $entityManagerFactory->getEntityManager();
 
 $entitiManager->persist($aluno);
-$aluno->setNome('Vinicius Dias');
 
 $entitiManager->flush();
