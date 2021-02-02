caa14690b2a3e0d1400cbecde30dcc31322d6a74
nely de faria <nelly.nf54@gmail.com>
2021-02-02 12:19:57 -0300

    Refatora código

--- php-doctrine/commands/atualizar-nome-aluno.php
+++ php-doctrine/commands/atualizar-nome-aluno.php
@@ -8,12 +8,11 @@ require_once __DIR__ .  '/../vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
 $entitiManager = $entityManagerFactory->getEntityManager();
-$alunoRepository = $entitiManager->getRepository(Aluno::class);
 
 $id = $argv[1];
 $novoNome = $argv[2];
 
-$aluno = $alunoRepository->find($id);
+$aluno = $entitiManager->find(Aluno::class, $id);
 $aluno->setNome($novoNome);
 
 $entitiManager->flush();
