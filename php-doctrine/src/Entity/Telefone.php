499736fa3db9606c92fce09c853aa2cf8eb258cb
nely de faria <nelly.nf54@gmail.com>
2021-02-03 18:30:27 -0300

    Adiciona classe Telefone

---
+++ php-doctrine/src/Entity/Telefone.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @Entity  */
+class Telefone
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private string $numero;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Aluno")
+     */
+    private $aluno;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Telefone
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getNumero(): string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): Telefone
+    {
+        $this->numero = $numero;
+        return $this;
+    }
+
+    public function getAluno()
+    {
+        return $this->aluno;
+    }
+
+    public function setAluno($aluno)
+    {
+        $this->aluno = $aluno;
+        return $this;
+    }
+
+}
