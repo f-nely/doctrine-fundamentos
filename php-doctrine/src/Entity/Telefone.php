8ffbd796fe00c825c2164803f0259b4b01e236f8
nely de faria <nelly.nf54@gmail.com>
2021-02-03 19:36:46 -0300

    Fazendo import necessários

--- php-doctrine/src/Entity/Telefone.php
+++ php-doctrine/src/Entity/Telefone.php
@@ -4,6 +4,10 @@
 namespace Alura\Doctrine\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
 
 /** @Entity  */
 class Telefone
