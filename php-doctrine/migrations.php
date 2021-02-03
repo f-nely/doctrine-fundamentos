30b72cdeeef6b28edcdaaaf4cb8db24dd6d31034
nely de faria <nelly.nf54@gmail.com>
2021-02-03 19:38:30 -0300

    Adiciona arquivo de configuração de migrations do doctrine

---
+++ php-doctrine/migrations.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/*return [
+    'table_storage' => [
+        'table_name' => 'Fundamentos Doctrine',
+        'version_column_name' => 'version',
+        'version_column_length' => 1024,
+        'executed_at_column_name' => 'executed_at',
+        'execution_time_column_name' => 'execution_time',
+    ],
+
+    'migrations_paths' => [
+        'Alura\\Doctrine\\Migrations' => '/src/Migrations',
+    ],
+
+    'all_or_nothing' => true,
+    'check_database_platform' => true,
+];*/
+
+return [
+    'name' => 'Fundamentos Doctrine',
+    'migrations_namespace' => 'Alura\\Doctrine\\Migrations',
+    'table_name' => 'doctrine_migration_versions',
+    'column_name' => 'version',
+    'column_length' => 14,
+    'executed_at_column_name' => 'executed_at',
+    'migrations_directory' => 'src/Migrations',
+    'all_or_nothing' => true,
+];
