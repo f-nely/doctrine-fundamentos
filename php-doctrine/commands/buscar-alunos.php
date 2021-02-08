0a9dc61a52c6602ac8a9d0cc06b9db20c81bbb44
nely de faria <nelly.nf54@gmail.com>
2021-02-08 19:13:46 -0300

    Atualiza crud

--- php-doctrine/commands/buscar-alunos.php
+++ php-doctrine/commands/buscar-alunos.php
@@ -1,6 +1,7 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -14,7 +15,17 @@ $alunoRepository = $entitiManager->getRepository(Aluno::class);
 $alunoList = $alunoRepository->findAll();
 
 foreach ($alunoList as $aluno) {
-    echo "ID: {$aluno->getId()}\n Nome: {$aluno->getNome()}\n\n";
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone){
+            return $telefone->getNumero();
+        })
+        ->toArray();
+
+    echo "ID: {$aluno->getId()}\nNome: {$aluno->getNome()}\n\n";
+    echo "Telefones: " . implode(',', $telefones);
+
+    echo "\n\n";
 }
 
 /*$nikita = $alunoRepository->find(2);
