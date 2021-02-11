9eddc477f220462802c3dd07d2f4a3e64df188e8
nely de faria <nelly.nf54@gmail.com>
2021-02-11 19:11:19 -0300

    Adiciona classe Curso

---
+++ php-doctrine/src/Entity/Curso.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToMany;
+
+/**
+ * @Entity
+ */
+class Curso
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private int $id;
+    /**
+     * @Column(type="string")
+     */
+    private string $nome;
+
+    /**
+     * @ManyToMany(targetEntity="Aluno", inversedBy="cursos")
+     */
+    private $alunos;
+
+    public function __construct()
+    {
+        $this->alunos = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): Curso
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function addAluno(Aluno $aluno)
+    {
+        if ($this->alunos->contains($aluno)) {
+            return $this;
+        }
+        $this->alunos->add($aluno);
+        $aluno->addCurso($this);
+        return $this;
+    }
+
+    public function getAlunos()
+    {
+        return $this->alunos;
+    }
+}
