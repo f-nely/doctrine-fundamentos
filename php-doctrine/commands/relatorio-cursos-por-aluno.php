988e3821e6a99da3de4023cb9c71649475381aab
nely de faria <nelly.nf54@gmail.com>
2021-02-11 20:04:43 -0300

    Adiciona arquivo relatorio-cursos-por-aluno.php

---
+++ php-doctrine/commands/relatorio-cursos-por-aluno.php
@@ -0,0 +1,41 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+use Doctrine\DBAL\Logging\DebugStack;
+
+require_once __DIR__ .  '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entitiManager = $entityManagerFactory->getEntityManager();
+$alunosRepositorio = $entitiManager->getRepository(Aluno::class);
+
+$debugStack = new DebugStack();
+$entitiManager->getConfiguration()->setSQLLogger($debugStack);
+
+/** @var Aluno[] $alunos */
+$alunos = $alunosRepositorio->findAll();
+
+foreach ($alunos as $aluno) {
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone) {
+            return $telefone->getNumero();
+        })
+        ->toArray();
+
+    echo "ID: {$aluno->getId()}\n";
+    echo "Nome: {$aluno->getNome()}\n";
+    echo "Telefones: " . implode(", ", $telefones) . "\n";
+    $cursos = $aluno->getCursos();
+
+    foreach ($cursos as $curso) {
+        echo "\tID Curso: {$curso->getId()}\n";
+        echo "\tCurso: {$curso->getNome()}\n";
+        echo "\n";
+    }
+    echo "\n";
+}
+
+print_r($debugStack);
