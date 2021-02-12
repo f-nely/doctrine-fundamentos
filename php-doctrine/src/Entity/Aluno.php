bf46e08d44a8f4e95759f51896123d300c9953d2
nely de faria <nelly.nf54@gmail.com>
2021-02-12 12:05:03 -0300

    Remove use e adiciona uma nova annotation

--- php-doctrine/src/Entity/Aluno.php
+++ php-doctrine/src/Entity/Aluno.php
@@ -6,7 +6,6 @@ namespace Alura\Doctrine\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
@@ -15,7 +14,7 @@ use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\OneToMany;
 
 /**
- * @Entity
+ * @Entity(repositoryClass="Alura\Doctrine\Repository\AlunoRepository")
  */
 class Aluno
 {
